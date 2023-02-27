2bf435c38199280d863f8c0ae4fd0ba7f448f113
CJewon <112820744+CJewon@users.noreply.github.com>
2023-02-27 17:59:27 +0900

    Study File Update

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.log(loginInput.value);
+  const value = loginInput.value;
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
