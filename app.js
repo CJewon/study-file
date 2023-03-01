52463e589833d6b4f77e131c6828056d7b33c3b6
CJewon <112820744+CJewon@users.noreply.github.com>
2023-03-01 09:33:20 +0900

    stuay project file update

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginSubmit() {
-  const value = loginInput.value;
-  console.log(username);
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
